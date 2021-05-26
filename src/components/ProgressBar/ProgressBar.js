a5dd0e4de4a536bbea68e93f9ebc0645b3cb331c
Juan Pablo Garcia Ripa <j.garcia.ripa@altoros.com>
2021-05-25 21:28:55 -0300

    first solution

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -6,7 +6,35 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return <Container size={size} >
+    <ProgressContainer>
+      <Progress value={value} role="progressbar" aria-valuenow={value} aria-valuemin="0" aria-valuemax="100"></Progress>
+    </ProgressContainer>
+  </Container>;
 };
 
+
+const SIZES = {
+  small:'8px',
+  medium:'12px',
+  large: '24px'
+};
+
+const Container = styled.div`
+  height: ${(props=> SIZES[props.size])};
+  border-radius: 8px;
+  ${props => props.size === "large" ? "padding: 4px;" : null}
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+`
+const ProgressContainer = styled.div`
+  height: 100%;
+  border-radius: 4px;
+  overflow: hidden;
+`
+
+const Progress = styled.div`
+  height: 100%;
+  width: ${props=>`${props.value}%`};
+  background-color: ${COLORS.primary};
+`
 export default ProgressBar;
